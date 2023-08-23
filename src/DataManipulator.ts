897a2e4889821319253dea196d1537eaf7e2b9a6
DEEPAK KUMAR <106382222+dpk0522@users.noreply.github.com>
2023-08-23 13:33:47 +0530

    Update DataManipulator.ts
    added new properties

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,34 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc:number,
+  price_def:number,
+  ratio:number,
+  upper_bond:number,
+  lower_bond:number,
+  trigger_alert:number|undefined,
   timestamp: Date,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
+   static generateRow(serverResponds: ServerRespond[]):Row {
+    const priceABC = (serverResponds[0].top_ask.price+serverResponds[0].top_bid.price)/2;
+    const priceDEF = (serverResponds[1].top_ask.price+serverResponds[1].top_bid.price)/2;
+    const ratio = priceABC/priceDEF;
+    const upper_bond=1+0.5;
+    const lower_bond=1-0.5;
+  
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+        price_abc:priceABC,
+        price_def:priceDEF,
+        ratio,
+        timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ?
+             serverResponds[0].timestamp : serverResponds[1].timestamp,
+        upper_bond:upper_bond,
+        lower_bond:lower_bond,
+        trigger_alert: (ratio > upper_bond||ratio  < lower_bond) ? ratio : undefined,
       };
-    })
+    }
   }
-}
+
